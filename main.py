85fd96c2082401aa2566979706a87ed64dacdd30
Ankit-47 <omkathet218@gmail.com>
2025-01-16 15:07:11 +0545

    fit : setup of fps  and clock for the events

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ import pymunk.pygame_util
 
 # Initialize Pygame
 pygame.init()
+WIDTH, HEIGHT = 1000, 800
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def run(window, width, height):
+    run = True
+    clock = pygame.time.Clock()
+    fps = 60
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        clock.tick(fps)
+       
