cfb01327d79efe820c34bd9b02b3501b089f2998
Ankit-47 <omkathet218@gmail.com>
2025-01-17 13:57:29 +0545

    fit :shape added for the ball

--- main.py
+++ main.py
@@ -9,15 +9,23 @@ pygame.init()
 WIDTH, HEIGHT = 1000, 800
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
-# def draw_window(space,window):
-#     window.fill((255,255,255))
-#     space.debug_draw(draw_options)  # Draw the objects in the space 
+
 
 def draw(space, window, draw_options):
     window.fill((255, 255, 255))
     space.debug_draw(draw_options)# Draw the objects in the space 
     pygame.display.update()
 
+def create_ball(space, radius, mass):
+    body = pymunk.Body() # Create a body with mass and moment of inertia
+    body.position = (300, 300) # Set the position of the body
+    shape = pymunk.Circle(body, radius) # Create a circle shape with the body and radius
+    shape.mass = mass
+    shape.color = (255, 0, 0, 100) # Set the color of the shape
+    shape.add(body, shape)  # Add the shape to the body
+    return shape
+
+    
 def run(window, width, height):
     run = True
     clock = pygame.time.Clock()
