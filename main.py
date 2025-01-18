4c629351251589a259d880e6307bf84c606252d5
Ankit-47 <omkathet218@gmail.com>
2025-01-18 19:12:45 +0545

    feat: ball falls down when you click on the boundary area

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ pygame.init()
 WIDTH, HEIGHT = 1000, 800
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def calculate_distance(p1, p2):
+    return math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
 
+def calculate_angle(p1, p2):    
+    return math.atan2(p2[1] - p1[1], p2[0] - p1[0])
 
 def draw(space, window, draw_options):
     window.fill((255, 255, 255))
@@ -34,7 +38,7 @@ def create_boundaries(space, width, height):
 
 def create_ball(space, radius, mass):
     body = pymunk.Body() # Create a body with mass and moment of inertia
-    body.position = (300, 300) # Set the position of the body
+    body.position = pos  # Set the position of the body
     shape = pymunk.Circle(body, radius) # Create a circle shape with the body and radius
     shape.mass = mass
     shape.elasticity = 0.95 # Set the elasticity of the shape
@@ -53,7 +57,9 @@ def run(window, width, height):
     space = pymunk.Space() # Create a space for the object to move in
     space.gravity = (0, 981) # Set the gravity
 
-    ball = create_ball(space, 30, 10) # Create a ball
+    pressed_pos = None
+    ball = None
+
     create_boundaries(space, width, height) # Create the boundaries
     draw_options = pymunk.pygame_util.DrawOptions(window) # Draw options for the objects
 
@@ -63,6 +69,9 @@ def run(window, width, height):
                 run = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
+                if not ball:
+                    ball = create_ball(space, 20, 1)
+                    pressed_pos = pygame.mouse.get_pos(space, 30, 10, pressed_pos)
                 ball.body.apply_impulse_at_local_point((10000, 0), (0, 0))
 
 
